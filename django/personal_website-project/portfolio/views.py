571e6e523fad9f598525cccb11170a2666aa0098
michael martyn <michaelryanmartyn@gmail.com>
2020-03-28 19:32:40 -0700

    added media to site

--- django/personal_website-project/portfolio/views.py
+++ django/personal_website-project/portfolio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def home(request):
+    projects = Project.objects.all() # grab all objects from database that are under Project
+    return render(request, 'portfolio/home.html', {'projects':projects})
