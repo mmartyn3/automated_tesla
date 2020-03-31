abbb6f53584d45877e249c648252eb502a7bf4d9
michael martyn <michaelryanmartyn@gmail.com>
2020-03-30 20:13:53 -0700

    added blog model and posts

--- django/personal_website-project/blog/models.py
+++ django/personal_website-project/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model): # required to generate table in database
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images/')
+    date = models.DateField()
