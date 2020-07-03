9f90681d6623c08593d3e68d195f67da5bafbc93
Michael Martyn <michael.martyn.92@gmail.com>
2020-07-02 18:11:41 -0700

    take down django projects

--- django/personal_website-project/portfolio/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Project
-
-# Create your views here.
-
-def home(request):
-    projects = Project.objects.all() # grab all objects from database that are under Project
-    return render(request, 'portfolio/home.html', {'projects':projects})
